9d268a6ea7a28ac28b03f2e9fbeb252453e19c56
nataliat <nataliat@NataliaW7.advahoo.local>
2017-04-05 13:21:08 +0300

    it's alive

--- main.js
+++ main.js
@@ -175,6 +175,11 @@ else{
 }
 
 
+
+
+
+})
+app.listen(1337);
 if (require.main === module) {
   console.log("Bot testing mode.");
   const client = new Wit({accessToken, actions});
@@ -183,7 +188,3 @@ if (require.main === module) {
 else{
 interactive(client);
 }
-
-
-})
-app.listen(1337);

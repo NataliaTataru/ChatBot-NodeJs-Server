5c5c26af576ba4f460101a3c755fbd99dab38236
nataliat <nataliat@NataliaW7.advahoo.local>
2017-04-06 13:23:36 +0300

    call5

--- main.js
+++ main.js
@@ -101,14 +101,14 @@ const actions = {
 //            console.log(response);
 //        }
 //    });
-        var resDB = request('GET','http://localhost:8080/RestTest/resources/com.airhacks.chatuser');
-        console.log("******************* DBBBBBB   "+resDB.getBody());
-        var xml = resDB.getBody();
-        parseString(xml, function (err, result) {
-            console.log("PAAAAAAAAAARRRRRRRRSSSEEEED#############   ");
-            console.log(result);
-        console.dir(result);
-});
+//        var resDB = request('GET','http://localhost:8080/RestTest/resources/com.airhacks.chatuser');
+//        console.log("******************* DBBBBBB   "+resDB.getBody());
+//        var xml = resDB.getBody();
+//        parseString(xml, function (err, result) {
+//            console.log("PAAAAAAAAAARRRRRRRRSSSEEEED#############   ");
+//            console.log(result);
+//        console.dir(result);
+//});
         
         var jsdom = require("jsdom");
 

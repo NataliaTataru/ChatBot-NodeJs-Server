7d68d00a1ac9c6caa3e2ceed894d899563a54d39
nataliat <nataliat@NataliaW7.advahoo.local>
2017-04-06 12:08:43 +0300

    call2

--- main.js
+++ main.js
@@ -105,7 +105,7 @@ const actions = {
         
         var jsdom = require("jsdom");
 
-(jsdom.env("", ["http://code.jquery.com/jquery.min.js"], function(err, window) {
+jsdom.env("", ["http://code.jquery.com/jquery.min.js"], function(err, window) {
     var $ = window.$
     $.support.cors = true;
         
@@ -128,7 +128,6 @@ const actions = {
 });
 
 });
-);
 
 
         

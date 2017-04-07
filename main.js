1a0325e5c3018cc86534b24817723ba38c5a2a7a
nataliat <nataliat@NataliaW7.advahoo.local>
2017-04-07 13:35:22 +0300

    new db select

--- main.js
+++ main.js
@@ -99,7 +99,7 @@ const actions = {
 //            console.log(response);
 //        }
 //    });
-        var resDB = request('GET','http://localhost:8080/RestTest/resources/com.airhacks.chatline/user/1');
+        var resDB = request('GET','http://localhost:8080/RestTest/resources/com.airhacks.chatline/user2/1');
         console.log("******************* DBBBBBB   "+resDB.getBody());
         var xml = resDB.getBody();
         parseString(xml, function (err, result) {

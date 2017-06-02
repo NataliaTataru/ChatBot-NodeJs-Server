3b1ef04022895da20beb0ac5f4f0526d5853b5c6
nataliat <nataliat@NataliaW7.advahoo.local>
2017-06-02 16:53:13 +0300

    changed back to port 3000

--- main.js
+++ main.js
@@ -259,6 +259,6 @@ interactive(client);
 //app.listen(process.env.PORT || 5000);
 ////app.listen(1337);
 app.use(express.static(__dirname + '/public'));
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 5000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });

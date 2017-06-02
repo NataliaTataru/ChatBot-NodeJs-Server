b4c1e5d3fe5204624025a1286af632609c58be30
nataliat <nataliat@NataliaW7.advahoo.local>
2017-06-02 16:45:35 +0300

    added index.html

--- main.js
+++ main.js
@@ -258,6 +258,7 @@ interactive(client);
 };
 //app.listen(process.env.PORT || 5000);
 ////app.listen(1337);
+app.use(express.static(__dirname + '/public'));
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
